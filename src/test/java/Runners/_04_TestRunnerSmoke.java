6bd501111e2a0321db0dbe7b0d6c772d1c849734
Erdem BÜKE <erdembke@gmail.com>
2023-05-02 20:46:39 +0300

    cucumber

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",
         features = {"src/test/java/FeatureFiles"},
         glue = "StepDefinitions",
-        plugin = {"html:target/site/cucumber-pretty.html"}
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
